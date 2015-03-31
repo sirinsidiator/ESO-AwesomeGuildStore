03a30bfa9cb95f4fa57c198934d36af023f36e86
sirinsidiator <insidiator@cmos.at>
2015-03-31 13:32:28 +0200

    added russian translation

--- src/Localization.lua
+++ src/Localization.lua
@@ -288,7 +288,87 @@ local localization = {
 		INVALID_STATE = "Ungültiger Händlerzustand.\nDas ist ein Fehler im Spiel und sollte bald behoben werden."
 	},
 	fr = {
-	}
+	},
+	ru = {
+		FILTER_SUBCATEGORY_JEWELRY = "Àèæóòepèü",
+		FILTER_SUBCATEGORY_GLYPHS = "Âìèñÿ",
+		SUBFILTER_WEAPON_ENCHANTMENT_LABEL = "Çaùapoáaîèü opóæèü",
+		SUBFILTER_WEAPON_TYPE_LABEL = "Òèï opóæèü",
+		SUBFILTER_ARMOR_TYPE_LABEL = "Òèï àpoîè",
+		SUBFILTER_ARMOR_ENCHANTMENT_LABEL = "Çaùapoáaîèü àpoîè",
+		SUBFILTER_JEWELRY_TYPE_LABEL = "Òèï àèæóòepèè",
+		SUBFILTER_JEWELRY_TRAIT_LABEL = "Ocoàeîîocòö àèæóòepèè",
+		SUBFILTER_JEWELRY_ENCHANTMENT_LABEL = "Çaùapoáaîèe àèæóòepèè",
+		SUBFILTER_MATERIAL_TYPE_LABEL = "Òèï íaòepèaìa",
+		SUBFILTER_INGREDIENT_TYPE_LABEL = "Òèï èâpeäèeîòa",
+		SUBFILTER_RUNE_TYPE_LABEL = "Òèï póîÿ",
+		SUBFILTER_GLYPH_TYPE_LABEL = "Òèï âìèña",
+
+		SEARCH_PREVIOUS_PAGE_LABEL = "Ïoêaçaòö ïpeä. còp.",
+		SEARCH_SHOW_MORE_LABEL = "Ïoêaçaòö àoìöúe peçóìöòaòoá",
+		RESET_ALL_FILTERS_LABEL = "Càpocèòö áce ñèìöòpÿ",
+		RESET_FILTER_LABEL_TEMPLATE = "Càpocèòö ñèìöòp %s",
+
+		CATEGORY_TITLE = "Êaòeâopèü",
+		SUBCATEGORY_TITLE = "Ïoäêaòeâopèü",
+
+		QUALITY_SELECTOR_TITLE = "Êaùecòáo:",
+
+		ITEM_NAME_QUICK_FILTER_LABEL = "Îaçáaîèe:",
+		ITEM_NAME_QUICK_FILTER_TEXT = "Oòoàpaòö ïo îaçáaîèô",
+		ITEM_NAME_QUICK_FILTER_RESET = "Càpocèòö oòàop ïo îaçáaîèô",
+
+		WARNING_SUBFILTER_LIMIT = "Îeìöçü oòoàpaòö àoìee ùeí ïo 8 ñèìöòpaí",
+
+		SEARCH_LIBRARY_TOGGLE_LABEL = "Àèàìèoòeêa ïoècêa",
+		SEARCH_LIBRARY_HISTORY_LABEL = "Ècòopèü",
+		SEARCH_LIBRARY_FAVORITES_LABEL = "Èçàpaîîoe",
+		SEARCH_LIBRARY_FAVORITE_BUTTON_ADD_TOOLTIP = "Äoàaáèòö á èçàpaîîoe",
+		SEARCH_LIBRARY_FAVORITE_BUTTON_REMOVE_TOOLTIP = "Óäaìèòö èç èçàpaîîoâo",
+		SEARCH_LIBRARY_EDIT_LABEL_BUTTON_TOOLTIP = "Ïepeèíeîoáaòö",
+		SEARCH_LIBRARY_DELETE_LABEL_BUTTON_TOOLTIP = "Óäaìèòö èç ècòopèè",
+		SEARCH_LIBRARY_MENU_OPEN_SETTINGS = "Oòêpÿòö îacòpoéêè aääoîa",
+		SEARCH_LIBRARY_MENU_CLEAR_HISTORY = "Oùècòèòö ècòopèô",
+		SEARCH_LIBRARY_MENU_CLEAR_FAVORITES = "Oùècòèòö èçàpaîîoe",
+		SEARCH_LIBRARY_MENU_UNDO_ACTION = "Oòíeîèòö ïocìeäîee äeécòáèe",
+		SEARCH_LIBRARY_MENU_UNLOCK_WINDOW = "Paçàìoêèpoáaòö oêîo",
+		SEARCH_LIBRARY_MENU_LOCK_WINDOW = "Çaàìoêèpoáaòö oêîo",
+		SEARCH_LIBRARY_MENU_RESET_WINDOW = "Càpocèòö ïoìoæeîèe oêîa",
+		SEARCH_LIBRARY_MENU_CLOSE_WINDOW = "Çaêpÿòö oêîo",
+
+		TOOLTIP_LESS_THAN = "íeîöúe ",
+		TOOLTIP_GREATER_THAN = "àoìöúe ",
+
+		SETTINGS_REQUIRES_RELOADUI_WARNING = "Àóäeò ïpèíeîeîo òoìöêo ïocìe ïepeçaâpóçêè UI",
+		SETTINGS_REPLACE_CATEGORY_FILTER_LABEL = "Ècïoìöçoáaòö óäoàîÿe êaòeâopèè oòàopa",
+		SETTINGS_REPLACE_CATEGORY_FILTER_DESCRIPTION = "Çaíeîüeò còaîäapòîÿé áÿàop íîoæecòáo êîoïoê",
+		SETTINGS_REPLACE_PRICE_FILTER_LABEL = "Ècïoìöçoáaòö óäoàîÿé ïoìçóîoê œeî",
+		SETTINGS_REPLACE_PRICE_FILTER_DESCRIPTION = "Äoàaáìüeò óäoàîÿé ïoìçóîoê äìü oòàopa ïo œeîaí",
+		SETTINGS_REPLACE_LEVEL_FILTER_LABEL = "Ècïoìöçoáaòö óäoàîÿé ïoìçóîoê ópoáîeé",
+		SETTINGS_REPLACE_LEVEL_FILTER_DESCRIPTION = "Äoàaáìüeò óäoàîÿé ïoìçóîoê äìü oòàopa ïo ópoáîüí",
+		SETTINGS_REPLACE_QUALITY_FILTER_LABEL = "Ècïoìöçoáaòö óäoàîÿé áÿàop êaùecòáa",
+		SETTINGS_REPLACE_QUALITY_FILTER_DESCRIPTION = "Çaíeîüeò còaîäapòîÿé áÿàop îa áÿàop ïo äèaïaçoîó",
+		SETTINGS_KEEP_FILTERS_ON_CLOSE_LABEL = "Çaïoíèîaòö ñèìöòpÿ íeæäó áèçèòaíè á íaâaçèî",
+		SETTINGS_KEEP_FILTERS_ON_CLOSE_DESCRIPTION = "Çaïoíèîaeò áÿàpaîîÿe áaíè ñèìöòpÿ è ïpè ïocìeäóôûeí áèçèòe á íaâaçèî ïocìeäîèe ècïoìöçoáaîîÿe ñèìöòpÿ óæe àóäóò áêìôùeîÿ",
+		SETTINGS_OLD_QUALITY_SELECTOR_BEHAVIOR_LABEL = "Ècïoìöçoáaòö còapÿé còèìö áÿàopa êaùecòáa",
+		SETTINGS_OLD_QUALITY_SELECTOR_BEHAVIOR_DESCRIPTION = "Ecìè áêìôùeîo, ìeáÿé è ïpaáÿé êìèê ïoáÿúaôò è ïoîèæaôò êaùecòáo, a äáoéîoé êìèê èìè êìèê c úèñòoí ïoçáoìüeò áÿàpaòö îecêoìöêo çîaùeîèé",
+		SETTINGS_DISPLAY_PER_UNIT_PRICE_LABEL = "Ïoêaçÿáaòö œeîó ça oäèî ïpeäíeò",
+		SETTINGS_DISPLAY_PER_UNIT_PRICE_DESCRIPTION = "Êoâäa áêìôùeîo, ïoêaçÿáaeò œeîó ça eäèîèœó òoáapa, áíecòo còoèíocòè còaêa",
+		SETTINGS_SORT_WITHOUT_SEARCH_LABEL = "Íeîüòö ïopüäoê àeç ïoècêa",
+		SETTINGS_SORT_WITHOUT_SEARCH_DESCRIPTION = "Ïoçáoìüeò íeîüòö ïopüäoê copòèpoáêè àeç îeoàxoäèíocòè çaïócêa îoáoâo ïoècêa. Òeêóûèe peçóìöòaòÿ ïoècêa èçíeîüòcü òoìöêo ïocìe póùîoâo ïoècêa.",
+		SETTINGS_KEEP_SORTORDER_ON_CLOSE_LABEL = "Çaïoíèîaòö copòèpoáó",
+		SETTINGS_KEEP_SORTORDER_ON_CLOSE_DESCRIPTION = "Çaïoíèîaeò ïopüäoê copòèpoáêè ïpeäíeòoá íeæäó áèçèòaíè á íaâaçî áíecòo eâo càpoca.",
+		SETTINGS_LIST_WITH_SINGLE_CLICK_LABEL = "Áÿcòaáìüòö oäîèí êìèêoí",
+		SETTINGS_LIST_WITH_SINGLE_CLICK_DESCRIPTION = "Áÿcòaáìüòö ïpeäíeò îa ïpoäaæó ïo oäîoíó êìèêó íÿúè.",
+		SETTINGS_SHOW_SEARCH_LIBRARY_TOOLTIPS_LABEL = "Ïoäcêaçêè á àèàìèoòeêe ïoècêa",
+		SETTINGS_SHOW_SEARCH_LIBRARY_TOOLTIPS_DESCRIPTION = "Êoâäa áêìôùeîo, àóäeò áÿáoäèòcü äeòaìöîaü èîñopíaœèü äì êaæäoé çaïècè á àèàìèoòeêe ïoècêa.",
+		SETTINGS_SHOW_TRADER_TOOLTIPS_LABEL = "Ïoäcêaçêa òopâoáœa",
+		SETTINGS_SHOW_TRADER_TOOLTIPS_DESCRIPTION = "Ïoêaçÿáaeò îaîüòoâo áaúèíè âèìöäèüíè òopâoáœa, êoâäa îaáoäèòe îa îaçáaîèe âèìöäèè á cïècêe âèìöäèé",
+		SETTINGS_AUTO_CLEAR_HISTORY_LABEL = "Aáòooùècòêa ècòopèè",
+		SETTINGS_AUTO_CLEAR_HISTORY_DESCRIPTION = "Aáòoíaòèùecêè oùèûaeò ácô ècòopèô ïpè ïepáoí c îaùaìa èâpoáoé ceccèè áèçèòe ê òopâoáœó. Áÿíoæeòe oòíeîèòö óäaìeîèe á íeîô ècòopèè ïoècêa",
+
+		INVALID_STATE = "Oúèàêa.\nËòo àaâ èâpÿ è oî äoìæeî àÿòö ïoïpaáìeî á àìèæaéúee ápeíü."
+	},
 }
 
 local language = GetCVar("language.2")
