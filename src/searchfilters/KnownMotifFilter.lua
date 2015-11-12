5c1de39ac51f0dd8b60c80be1ccc7e10f41f50e3
sirinsidiator <insidiator@cmos.at>
2015-11-12 23:57:48 +0100

    fixed mercenary motif not being filtered correctly

--- src/searchfilters/KnownMotifFilter.lua
+++ src/searchfilters/KnownMotifFilter.lua
@@ -34,8 +34,26 @@ function KnownMotifFilter:BeforeRebuildSearchResultsPage(tradingHouseWrapper)
 	return (self.showUnknown ~= self.showKnown)
 end
 
+-- TODO: this is just a workaround until ZOS fixes the mercenary motif books; remove it once it works
+local MERCENARY_ACHIEVEMENT_ID = 1348
+local MERCENARY_BOOK_ITEM_ID = 64715
+local MERCENARY_CHAPTER_START_ID = 64716
+local MERCENARY_CHAPTER_END_ID = 64729
+local function IsMercenaryMotifKnown(link)
+	local itemId = select(3, zo_strsplit(":", link))
+	itemId = tonumber(itemId)
+	if(itemId < MERCENARY_BOOK_ITEM_ID or itemId > MERCENARY_CHAPTER_END_ID) then return false end
+	if(IsAchievementComplete(MERCENARY_ACHIEVEMENT_ID)) then return true end
+	if(itemId ~= MERCENARY_BOOK_ITEM_ID) then
+	local index = itemId - MERCENARY_CHAPTER_START_ID + 1
+	local _, numCompleted, numRequired = GetAchievementCriterion(MERCENARY_ACHIEVEMENT_ID, index)
+		return numCompleted == numRequired
+	end
+	return false
+end
+
 function KnownMotifFilter:FilterPageResult(index, icon, name, quality, stackCount, sellerName, timeRemaining, purchasePrice)
 	local itemLink = GetTradingHouseSearchResultItemLink(index, LINK_STYLE_BRACKETS)
-	local isKnown = IsItemLinkBookKnown(itemLink)
+	local isKnown = IsItemLinkBookKnown(itemLink) or IsMercenaryMotifKnown(itemLink)
 	return (self.showUnknown and not isKnown) or (self.showKnown and isKnown)
 end
