53f398099378e1f9a269add7a2b1455f9ac30269
sirinsidiator <insidiator@cmos.at>
2016-03-05 17:21:46 +0100

    fixed researchable trait filter not separating research lines

--- src/searchfilters/ResearchableTraitsFilter.lua
+++ src/searchfilters/ResearchableTraitsFilter.lua
@@ -15,7 +15,6 @@ function ResearchableTraitsFilter:ApplyFilterValues(filterArray)
 -- do nothing here as we want to filter on the result page
 end
 
-
 function ResearchableTraitsFilter:BeforeRebuildSearchResultsPage(tradingHouseWrapper)
 	self.showUnknown = false
 	self.showKnown = false
@@ -34,15 +33,71 @@ function ResearchableTraitsFilter:BeforeRebuildSearchResultsPage(tradingHouseWra
 	return (self.showUnknown ~= self.showKnown)
 end
 
+local EQUIP_TYPE_BY_RESEARCH_LINE = {
+	[1] = EQUIP_TYPE_CHEST, -- light armor
+	[2] = EQUIP_TYPE_FEET,
+	[3] = EQUIP_TYPE_HAND,
+	[4] = EQUIP_TYPE_HEAD,
+	[5] = EQUIP_TYPE_LEGS,
+	[6] = EQUIP_TYPE_SHOULDERS,
+	[7] = EQUIP_TYPE_WAIST,
+	[8] = EQUIP_TYPE_CHEST, -- medium and heavy armor
+	[9] = EQUIP_TYPE_FEET,
+	[10] = EQUIP_TYPE_HAND,
+	[11] = EQUIP_TYPE_HEAD,
+	[12] = EQUIP_TYPE_LEGS,
+	[13] = EQUIP_TYPE_SHOULDERS,
+	[14] = EQUIP_TYPE_WAIST,
+}
+
+local BLACKSMITHING_WEAPON_TYPE_BY_RESEARCH_LINE = {
+	[1] = WEAPONTYPE_AXE,
+	[2] = WEAPONTYPE_HAMMER,
+	[3] = WEAPONTYPE_SWORD,
+	[4] = WEAPONTYPE_TWO_HANDED_AXE,
+	[5] = WEAPONTYPE_TWO_HANDED_HAMMER,
+	[6] = WEAPONTYPE_TWO_HANDED_SWORD,
+	[7] = WEAPONTYPE_DAGGER,
+}
+
+local WOODWORKING_WEAPON_TYPE_BY_RESEARCH_LINE = {
+	[1] = WEAPONTYPE_BOW,
+	[2] = WEAPONTYPE_FIRE_STAFF,
+	[3] = WEAPONTYPE_FROST_STAFF,
+	[4] = WEAPONTYPE_LIGHTNING_STAFF,
+	[5] = WEAPONTYPE_HEALING_STAFF,
+	[6] = WEAPONTYPE_SHIELD,
+}
+
+local function GetResearchLineKey(craftingSkillType, researchLineIndex)
+	if(craftingSkillType == CRAFTING_TYPE_BLACKSMITHING) then
+		if(researchLineIndex <= 7) then
+			return ("%d_%d"):format(ITEMTYPE_WEAPON, BLACKSMITHING_WEAPON_TYPE_BY_RESEARCH_LINE[researchLineIndex])
+		else
+			return ("%d_%d_%d"):format(ITEMTYPE_ARMOR, ARMORTYPE_HEAVY, EQUIP_TYPE_BY_RESEARCH_LINE[researchLineIndex])
+		end
+	elseif(craftingSkillType == CRAFTING_TYPE_WOODWORKING) then
+		return ("%d_%d"):format(ITEMTYPE_WEAPON, WOODWORKING_WEAPON_TYPE_BY_RESEARCH_LINE[researchLineIndex])
+	elseif(craftingSkillType == CRAFTING_TYPE_CLOTHIER) then
+		if(researchLineIndex <= 7) then
+			return ("%d_%d_%d"):format(ITEMTYPE_ARMOR, ARMORTYPE_LIGHT, EQUIP_TYPE_BY_RESEARCH_LINE[researchLineIndex])
+		else
+			return ("%d_%d_%d"):format(ITEMTYPE_ARMOR, ARMORTYPE_MEDIUM, EQUIP_TYPE_BY_RESEARCH_LINE[researchLineIndex])
+		end
+	end
+end
+
 local traitDictionary = {}
 local function BuildTraitDictionary(craftingSkillType)
-	if(not traitDictionary[craftingSkillType]) then traitDictionary[craftingSkillType] = {} end
 	for researchLineIndex = 1, GetNumSmithingResearchLines(craftingSkillType) do
 		local _,_, numTraits = GetSmithingResearchLineInfo(craftingSkillType, researchLineIndex)
+		local key = GetResearchLineKey(craftingSkillType, researchLineIndex)
+		if(not traitDictionary[key]) then traitDictionary[key] = {} end
+
 		for traitIndex = 1, numTraits do
 			local traitType, _, known = GetSmithingResearchLineTraitInfo(craftingSkillType, researchLineIndex, traitIndex)
 
-			traitDictionary[craftingSkillType][traitType] = {
+			traitDictionary[key][traitType] = {
 				known = known,
 				researching = (GetSmithingResearchLineTraitTimes(craftingSkillType, researchLineIndex, traitIndex) ~= nil),
 				craftingSkillType = craftingSkillType,
@@ -58,23 +113,16 @@ BuildTraitDictionary(CRAFTING_TYPE_WOODWORKING)
 
 local function GetTraitDictionaryForItemLink(itemLink)
 	local itemType = GetItemLinkItemType(itemLink)
+	local key
 	if(itemType == ITEMTYPE_ARMOR) then
+		local equipType = GetItemLinkEquipType(itemLink)
 		local armorType = GetItemLinkArmorType(itemLink)
-		if(armorType == ARMORTYPE_HEAVY) then
-			return traitDictionary[CRAFTING_TYPE_BLACKSMITHING]
-		elseif(armorType == ARMORTYPE_MEDIUM or armorType == ARMORTYPE_LIGHT) then
-			return traitDictionary[CRAFTING_TYPE_CLOTHIER]
-		end
+		key = ("%d_%d_%d"):format(itemType, armorType, equipType)
 	elseif(itemType == ITEMTYPE_WEAPON) then
 		local weaponType = GetItemLinkWeaponType(itemLink)
-		if(weaponType == WEAPONTYPE_BOW or weaponType == WEAPONTYPE_FIRE_STAFF
-			or weaponType == WEAPONTYPE_FROST_STAFF or weaponType == WEAPONTYPE_HEALING_STAFF
-			or weaponType == WEAPONTYPE_LIGHTNING_STAFF or weaponType == WEAPONTYPE_SHIELD) then
-			return traitDictionary[CRAFTING_TYPE_WOODWORKING]
-		elseif(weaponType ~= WEAPONTYPE_NONE) then
-			return traitDictionary[CRAFTING_TYPE_BLACKSMITHING]
-		end
+		key = ("%d_%d"):format(itemType, weaponType)
 	end
+	return traitDictionary[key]
 end
 
 local function IsItemLinkTraitKnown(itemLink)
