220e9334e45f0bd951427392b7b72bcc80252e08
sirinsidiator <insidiator@cmos.at>
2016-06-09 00:14:34 +0200

    code cleanup

--- src/wrappers/SellTabWrapper.lua
+++ src/wrappers/SellTabWrapper.lua
@@ -7,79 +7,19 @@ local SellTabWrapper = ZO_Object:Subclass()
 AwesomeGuildStore.SellTabWrapper = SellTabWrapper
 
 local iconMarkup = string.format("|t%u:%u:%s|t", 16, 16, "EsoUI/Art/currency/currency_gold.dds")
+local POST_ITEM_PENDING_UPDATE_NAMESPACE = "AwesomeGuildStorePostPendingItemPendingUpdate"
 local SUPPRESS_PRICE_PER_PIECE_UPDATE = true
-
-function SellTabWrapper:New(saveData)
-    local wrapper = ZO_Object.New(self)
-    wrapper:Initialize(saveData)
-    return wrapper
-end
-
-function SellTabWrapper:Initialize(saveData)
-    self.saveData = saveData
-    self:ClearPendingItem()
-
-    self.lastSoldStackCount = saveData.lastSoldStackCount or {}
-    saveData.lastSoldStackCount = self.lastSoldStackCount
-    self.lastSoldPricePerUnit = saveData.lastSoldPricePerUnit or {}
-    saveData.lastSoldPricePerUnit = self.lastSoldPricePerUnit
-
-    if(saveData.disableCustomSellTabFilter) then
-        self.customFilterDisabled = true
-    else
-        self.customFilterDisabled = false
-        local libCIF = LibStub:GetLibrary("libCommonInventoryFilters", LibStub.SILENT)
-        libCIF:disableGuildStoreSellFilters()
-    end
-    self.currentInventoryFragment = INVENTORY_FRAGMENT
-end
-
-function SellTabWrapper:RunInitialSetup(tradingHouseWrapper)
-    self:InitializeQuickListing(tradingHouseWrapper)
-    self:InitializeListingInput(tradingHouseWrapper)
-    self:InitializeListedNotification(tradingHouseWrapper)
-    self:InitializeCraftingBag(tradingHouseWrapper)
-end
-
-function SellTabWrapper:InitializeQuickListing(tradingHouseWrapper)
-    self.interceptInventoryItemClicks = false
-
-    ZO_PreHook("ZO_InventorySlot_OnSlotClicked", function(inventorySlot, button)
-        if(self.interceptInventoryItemClicks and self.saveData.listWithSingleClick and button == 1) then
-            ZO_InventorySlot_DoPrimaryAction(inventorySlot)
-            return true
-        end
-    end)
-end
-
-function SellTabWrapper:UpdateListing()
-    local tradingHouse = self.tradingHouse
-    local price = math.ceil(self.currentStackCount * self.currentPricePerUnit)
-    ZO_ItemSlot_SetupSlot(tradingHouse.m_pendingItem, self.currentStackCount, self.pendingIcon)
-    tradingHouse:SetPendingPostPrice(price, SUPPRESS_PRICE_PER_PIECE_UPDATE)
-end
-
-function SellTabWrapper:SetQuantity(value, skipUpdateSlider)
-    self.currentStackCount = math.max(1, math.min(self.pendingStackCount, value))
-    if(not skipUpdateSlider) then self.quantitySlider:UpdateValue() end
-    self:UpdateListing()
-end
-
-function SellTabWrapper:GetQuantity()
-    return self.currentStackCount
-end
-
-function SellTabWrapper:SetUnitPrice(value, skipUpdateSlider)
-    self.currentPricePerUnit = math.max(0, value)
-    if(not skipUpdateSlider) then self.ppuSlider:UpdateValue() end
-    self:UpdateListing()
-end
-
-function SellTabWrapper:GetUnitPrice()
-    return self.currentPricePerUnit
-end
-
+local SKIP_UPDATE_SLIDER = true
 local SLIDER_HEIGHT = 54
+local LISTING_INPUT_BUTTON_SIZE = 24
+local INVENTORY_BUTTON_SIZE = 64
+local FULL_QUANTITY_TEXTURE = "EsoUI/Art/Inventory/inventory_tabIcon_all_%s.dds"
+local LAST_SOLD_QUANTITY_TEXTURE = "EsoUI/Art/Campaign/campaign_tabIcon_history_%s.dds"
+local DEFAULT_PRICE_TEXTURE = "EsoUI/Art/Bank/bank_tabIcon_gold_%s.dds"
+local LAST_SELL_PRICE_TEXTURE = "EsoUI/Art/Campaign/campaign_tabIcon_history_%s.dds"
+local AVERAGE_PRICE_TEXTURE = "EsoUI/Art/Guild/tabIcon_roster_%s.dds"
+local INVENTORY_TEXTURE = "EsoUI/Art/Inventory/inventory_tabIcon_items_%s.dds"
+local CRAFTING_BAG_TEXTURE = "EsoUI/Art/Inventory/inventory_tabIcon_Craftbag_%s.dds"
 
 local function ShowSlider(self)
     self:SetHidden(false)
@@ -109,9 +49,6 @@ local function CreateSlider(container, data, name)
     container:SetDimensions(203, SLIDER_HEIGHT)
     container.container:SetWidth(190)
 
-    --    slider.minText:SetHidden(true)
-    --    slider.maxText:SetHidden(true)
-
     local slider = container.slider
     slider:SetAnchor(TOPRIGHT, nil, nil, -60)
     slider:SetHeight(16)
@@ -139,6 +76,14 @@ local function CreateSlider(container, data, name)
     container.SetMinMax = SetMinMax
 
     return container
+end
+
+local function MoveItem(sourceBag, sourceSlot, destBag, destSlot, stackCount)
+    if IsProtectedFunction("RequestMoveItem") then
+        CallSecureProtected("RequestMoveItem", sourceBag, sourceSlot, destBag, destSlot, stackCount)
+    else
+        RequestMoveItem(sourceBag, sourceSlot, destBag, destSlot, stackCount)
+    end
 end
 
 local function GetMasterMerchantPrice(itemLink)
@@ -193,24 +138,65 @@ local function GetItemIdentifier(itemLink)
     end
 end
 
+function SellTabWrapper:New(saveData)
+    local wrapper = ZO_Object.New(self)
+    wrapper:Initialize(saveData)
+    return wrapper
+end
+
+function SellTabWrapper:Initialize(saveData)
+    self.saveData = saveData
+    self:ClearPendingItem()
+
+    self.lastSoldStackCount = saveData.lastSoldStackCount or {}
+    saveData.lastSoldStackCount = self.lastSoldStackCount
+    self.lastSoldPricePerUnit = saveData.lastSoldPricePerUnit or {}
+    saveData.lastSoldPricePerUnit = self.lastSoldPricePerUnit
+
+    if(saveData.disableCustomSellTabFilter) then
+        self.customFilterDisabled = true
+    else
+        self.customFilterDisabled = false
+        local libCIF = LibStub:GetLibrary("libCommonInventoryFilters", LibStub.SILENT)
+        libCIF:disableGuildStoreSellFilters()
+    end
+    self.currentInventoryFragment = INVENTORY_FRAGMENT
+end
+
+function SellTabWrapper:RunInitialSetup(tradingHouseWrapper)
+    self:InitializeQuickListing(tradingHouseWrapper)
+    self:InitializeListingInput(tradingHouseWrapper)
+    self:InitializeListedNotification(tradingHouseWrapper)
+    self:InitializeCraftingBag(tradingHouseWrapper)
+end
+
+function SellTabWrapper:InitializeQuickListing(tradingHouseWrapper)
+    self.interceptInventoryItemClicks = false
+
+    ZO_PreHook("ZO_InventorySlot_OnSlotClicked", function(inventorySlot, button)
+        if(self.interceptInventoryItemClicks and self.saveData.listWithSingleClick and button == 1) then
+            ZO_InventorySlot_DoPrimaryAction(inventorySlot)
+            return true
+        end
+    end)
+end
+
 function SellTabWrapper:InitializeListingInput(tradingHouseWrapper)
     local tradingHouse = tradingHouseWrapper.tradingHouse
     self.tradingHouse = tradingHouse
+
     local container = tradingHouse.m_postItems:GetNamedChild("FormInvoice")
-    self.sellPriceControl = container:GetNamedChild("SellPrice")
     container.data = {} -- required for LAM
+    self.sellPriceControl = container:GetNamedChild("SellPrice")
 
     local quantitySlider = CreateSlider(container, {
         name = L["SELL_QUANTITY_SLIDER_LABEL"],
         getFunc = function() return self.currentStackCount end,
-        setFunc = function(value) self:SetQuantity(value, true) end,
+        setFunc = function(value) self:SetQuantity(value, SKIP_UPDATE_SLIDER) end,
     }, "AwesomeGuildStoreFormInvoiceQuantitySlider")
     quantitySlider:SetAnchor(TOPLEFT, container, TOPLEFT, 0, 0)
     self.quantitySlider = quantitySlider
 
-    local LISTING_INPUT_BUTTON_SIZE = 24
-    local FULL_QUANTITY_TEXTURE = "EsoUI/Art/Inventory/inventory_tabIcon_all_%s.dds"
-    local LAST_SOLD_QUANTITY_TEXTURE = "EsoUI/Art/Campaign/campaign_tabIcon_history_%s.dds"
     local fullQuantityButton = ToggleButton:New(quantitySlider, "$(parent)FullQuantityButton", FULL_QUANTITY_TEXTURE, 0, 0, LISTING_INPUT_BUTTON_SIZE, LISTING_INPUT_BUTTON_SIZE, L["SELL_FULL_QUANTITY_BUTTON_LABEL"])
     fullQuantityButton.control:ClearAnchors()
     fullQuantityButton.control:SetAnchor(TOPRIGHT, quantitySlider, TOPRIGHT, 0, 0)
@@ -230,15 +216,11 @@ function SellTabWrapper:InitializeListingInput(tradingHouseWrapper)
     local ppuSlider = CreateSlider(container, {
         name = L["SELL_PPU_SLIDER_LABEL"],
         getFunc = function() return tonumber(string.format("%.2f", self.currentPricePerUnit)) end,
-        setFunc = function(value) self:SetUnitPrice(value, true) end,
+        setFunc = function(value) self:SetUnitPrice(value, SKIP_UPDATE_SLIDER) end,
         decimals = 2,
     }, "AwesomeGuildStoreFormInvoicePPUSlider")
     ppuSlider:SetAnchor(TOPLEFT, quantitySlider, BOTTOMLEFT, 0, 10)
     self.ppuSlider = ppuSlider
-
-    local DEFAULT_PRICE_TEXTURE = "EsoUI/Art/Bank/bank_tabIcon_gold_%s.dds"
-    local LAST_SELL_PRICE_TEXTURE = "EsoUI/Art/Campaign/campaign_tabIcon_history_%s.dds"
-    local AVERAGE_PRICE_TEXTURE = "EsoUI/Art/Guild/tabIcon_roster_%s.dds"
 
     local buttonContainer = container:CreateControl("AwesomeGuildStoreFormInvoicePriceButtons", CT_CONTROL)
     buttonContainer:SetAnchor(TOPRIGHT, ppuSlider, TOPRIGHT, 0, 0)
@@ -302,10 +284,6 @@ end
 function SellTabWrapper:InitializeCraftingBag(tradingHouseWrapper)
     local tradingHouse = tradingHouseWrapper.tradingHouse
 
-    -- create toggle buttons
-    local INVENTORY_TEXTURE = "EsoUI/Art/Inventory/inventory_tabIcon_items_%s.dds"
-    local CRAFTING_BAG_TEXTURE = "EsoUI/Art/Inventory/inventory_tabIcon_Craftbag_%s.dds"
-    local INVENTORY_BUTTON_SIZE = 64
     local container = tradingHouse.m_postItems:CreateControl("AwesomeGuildStoreSellTabButtons", CT_CONTROL)
     container:SetAnchor(BOTTOM, tradingHouse.m_postItems, TOP, 0, 10)
     container:SetDimensions(INVENTORY_BUTTON_SIZE * 2, INVENTORY_BUTTON_SIZE)
@@ -316,7 +294,6 @@ function SellTabWrapper:InitializeCraftingBag(tradingHouseWrapper)
         if(not button:IsPressed()) then
             craftbagButton:Release(true)
             self:SetCurrentInventory(BAG_BACKPACK)
-            self:ClearPendingItem()
             return true
         end
     end
@@ -325,28 +302,10 @@ function SellTabWrapper:InitializeCraftingBag(tradingHouseWrapper)
         if(not button:IsPressed()) then
             inventoryButton:Release(true)
             self:SetCurrentInventory(BAG_VIRTUAL)
-            self:ClearPendingItem()
             return true
         end
     end
     craftbagButton.HandleRelease = function(button, doRelease) return doRelease end
-
-    local function MoveItem(sourceBag, sourceSlot, destBag, destSlot, stackCount)
-        if IsProtectedFunction("RequestMoveItem") then
-            CallSecureProtected("RequestMoveItem", sourceBag, sourceSlot, destBag, destSlot, stackCount)
-        else
-            RequestMoveItem(sourceBag, sourceSlot, destBag, destSlot, stackCount)
-        end
-    end
-
-    local function IsItemAlreadyBeingPosted(inventorySlot)
-        if ZO_InventorySlot_GetType(inventorySlot) == SLOT_TYPE_TRADING_HOUSE_POST_ITEM then
-            return self.pendingStackCount > 0
-        end
-
-        local bag, slot = ZO_Inventory_GetBagAndIndex(inventorySlot)
-        return self.pendingStackCount > 0 and bag == self.pendingBagId and slot == self.pendingSlotIndex
-    end
 
     local function TryInitiatingItemPost(inventorySlot)
         local bag, slot = ZO_Inventory_GetBagAndIndex(inventorySlot)
@@ -380,7 +339,7 @@ function SellTabWrapper:InitializeCraftingBag(tradingHouseWrapper)
     -- prepare AddSlotAction in order to redirect the action
     ZO_InventorySlotActions.AddSlotAction = function(slotActions, actionStringId, actionCallback, actionType, visibilityFunction, options)
         if(actionStringId == SI_ITEM_ACTION_REMOVE_ITEMS_FROM_CRAFT_BAG and TRADING_HOUSE:IsAtTradingHouse()) then
-            if(IsItemAlreadyBeingPosted(currentInventorySlot)) then
+            if(self:IsItemAlreadyBeingPosted(currentInventorySlot)) then
                 actionStringId = SI_TRADING_HOUSE_REMOVE_PENDING_POST
                 actionCallback = function()
                     self:ClearPendingItem()
@@ -406,7 +365,6 @@ function SellTabWrapper:InitializeCraftingBag(tradingHouseWrapper)
         end
     end)
 
-    local POST_ITEM_PENDING_UPDATE_NAMESPACE = "AwesomeGuildStorePostPendingItemPendingUpdate"
     tradingHouseWrapper:Wrap("PostPendingItem", function(originalPostPendingItem, tradingHouse)
         if(self.requiresTempSlot) then
             if(self.currentSellPrice <= 0) then return end
@@ -459,6 +417,42 @@ function SellTabWrapper:InitializeCraftingBag(tradingHouseWrapper)
             lastGuildId = guildId
         end
     end)
+end
+
+function SellTabWrapper:IsItemAlreadyBeingPosted(inventorySlot)
+    if ZO_InventorySlot_GetType(inventorySlot) == SLOT_TYPE_TRADING_HOUSE_POST_ITEM then
+        return self.pendingStackCount > 0
+    end
+
+    local bag, slot = ZO_Inventory_GetBagAndIndex(inventorySlot)
+    return self.pendingStackCount > 0 and bag == self.pendingBagId and slot == self.pendingSlotIndex
+end
+
+function SellTabWrapper:UpdateListing()
+    local tradingHouse = self.tradingHouse
+    local price = math.ceil(self.currentStackCount * self.currentPricePerUnit)
+    ZO_ItemSlot_SetupSlot(tradingHouse.m_pendingItem, self.currentStackCount, self.pendingIcon)
+    tradingHouse:SetPendingPostPrice(price, SUPPRESS_PRICE_PER_PIECE_UPDATE)
+end
+
+function SellTabWrapper:SetQuantity(value, skipUpdateSlider)
+    self.currentStackCount = math.max(1, math.min(self.pendingStackCount, value))
+    if(not skipUpdateSlider) then self.quantitySlider:UpdateValue() end
+    self:UpdateListing()
+end
+
+function SellTabWrapper:GetQuantity()
+    return self.currentStackCount
+end
+
+function SellTabWrapper:SetUnitPrice(value, skipUpdateSlider)
+    self.currentPricePerUnit = math.max(0, value)
+    if(not skipUpdateSlider) then self.ppuSlider:UpdateValue() end
+    self:UpdateListing()
+end
+
+function SellTabWrapper:GetUnitPrice()
+    return self.currentPricePerUnit
 end
 
 function SellTabWrapper:UpdateTempSlot()
@@ -573,6 +567,7 @@ function SellTabWrapper:ResetSalesCategoryFilter()
 end
 
 function SellTabWrapper:SetCurrentInventory(bagId)
+    self:ClearPendingItem()
     SCENE_MANAGER:RemoveFragment(self.currentInventoryFragment)
     if(bagId == BAG_BACKPACK) then
         self.currentInventoryFragment = INVENTORY_FRAGMENT
