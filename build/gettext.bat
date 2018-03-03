607b3c652e5ade843142abcc415c18b5d0f3a014
sirinsidiator <insidiator@cmos.at>
2018-03-03 19:11:08 +0100

    synced translations with zanata and added Brazilian Portuguese

--- build/gettext.bat
+++ build/gettext.bat
@@ -2,6 +2,6 @@
 echo Generating file list..
 dir ..\src\*.lua /L /B /S > %TEMP%\listfile.txt
 echo Generating .POT file...
-D:\programs\gettext\bin\xgettext -kgettext --from-code utf-8 --copyright-holder=sirinsidiator --package-name=AwesomeGuildStore --msgid-bugs-address=insidiator@cmos.at -cTRANSLATORS: -o ..\translations\messages.pot -L Lua --no-wrap -D ..\src -f %TEMP%\listfile.txt
+E:\programs\gettext\bin\xgettext -kgettext --from-code utf-8 --copyright-holder=sirinsidiator --package-name=AwesomeGuildStore --msgid-bugs-address=insidiator@cmos.at -cTRANSLATORS: -o ..\translations\messages.pot -L Lua --no-wrap -D ..\src -f %TEMP%\listfile.txt
 echo Done.
 del %TEMP%\listfile.txt
