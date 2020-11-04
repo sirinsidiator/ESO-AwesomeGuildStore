3b2cc1edd72961f32e9b8133ff5791149d3a0202
sirinsidiator <insidiator@cmos.at>
2020-11-04 17:19:04 +0100

    updated collectible ownership filter to handle item sets too

--- src/backend/filter/CollectibleOwnershipFilter.lua
+++ src/backend/filter/CollectibleOwnershipFilter.lua
@@ -8,9 +8,25 @@ local SUB_CATEGORY_ID = AGS.data.SUB_CATEGORY_ID
 
 local gettext = AGS.internal.gettext
 
-local function IsItemCollected(itemLink)
+local function IsSetItemCollected(itemLink, setId)
+    if not IsItemLinkSetCollectionPiece(itemLink) then return nil end
+    local setId = select(6, GetItemLinkSetInfo(itemLink, false))
+    local slot = GetItemLinkItemSetCollectionSlot(itemLink)
+    return IsItemSetCollectionSlotUnlocked(setId, slot)
+end
+
+local function IsContainerCollected(itemLink)
     local collectibleId = GetItemLinkContainerCollectibleId(itemLink)
+    if collectibleId == 0 then return nil end
     return IsCollectibleUnlocked(collectibleId)
+end
+
+local function IsItemCollected(itemLink)
+    if GetItemLinkEquipType(itemLink) == EQUIP_TYPE_INVALID then
+        return IsContainerCollected(itemLink)
+    else
+        return IsSetItemCollected(itemLink)
+    end
 end
 
 local CollectibleOwnershipFilter = MultiChoiceFilterBase:Subclass()
@@ -25,24 +41,39 @@ function CollectibleOwnershipFilter:Initialize()
     MultiChoiceFilterBase.Initialize(self, FILTER_ID.COLLECTIBLE_OWNERSHIP_FILTER, FilterBase.GROUP_LOCAL, gettext("Collectible Ownership"), {
         {
             id = false,
-            -- TRANSLATORS: tooltip text for the collectible ownership filter
-            label = gettext("Uncollected"),
+            label = GetString(SI_ITEM_FORMAT_STR_SET_COLLECTION_PIECE_LOCKED),
             icon = "EsoUI/Art/Campaign/overview_indexIcon_bonus_%s.dds",
         },
         {
             id = true,
-            -- TRANSLATORS: tooltip text for the collectible ownership filter
-            label = gettext("Collected"),
+            label = GetString(SI_ITEM_FORMAT_STR_SET_COLLECTION_PIECE_UNLOCKED),
             icon = "EsoUI/Art/Journal/journal_tabIcon_loreLibrary_%s.dds",
         },
     })
     self:SetEnabledSubcategories({
+        [SUB_CATEGORY_ID.WEAPONS_ALL] = true,
+        [SUB_CATEGORY_ID.WEAPONS_ONE_HANDED] = true,
+        [SUB_CATEGORY_ID.WEAPONS_TWO_HANDED] = true,
+        [SUB_CATEGORY_ID.WEAPONS_BOW] = true,
+        [SUB_CATEGORY_ID.WEAPONS_DESTRUCTION_STAFF] = true,
+        [SUB_CATEGORY_ID.WEAPONS_RESTORATION_STAFF] = true,
+        [SUB_CATEGORY_ID.ARMOR_ALL] = true,
+        [SUB_CATEGORY_ID.ARMOR_HEAVY] = true,
+        [SUB_CATEGORY_ID.ARMOR_MEDIUM] = true,
+        [SUB_CATEGORY_ID.ARMOR_LIGHT] = true,
+        [SUB_CATEGORY_ID.ARMOR_SHIELD] = true,
+        [SUB_CATEGORY_ID.JEWELRY_ALL] = true,
+        [SUB_CATEGORY_ID.JEWELRY_RING] = true,
+        [SUB_CATEGORY_ID.JEWELRY_NECK] = true,
         [SUB_CATEGORY_ID.CONSUMABLE_CONTAINER] = true,
     })
 end
 
 function CollectibleOwnershipFilter:FilterLocalResult(itemData)
     local id = IsItemCollected(itemData.itemLink)
+    -- don't show items without a collection when the filter is active
+    if id == nil then return false end
+
     local value = self.valueById[id]
     return self.localSelection[value]
 end
