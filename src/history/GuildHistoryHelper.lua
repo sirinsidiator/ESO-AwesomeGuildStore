af3b92e8b43bac4d4b5c58ded5f1419ddb916f24
sirinsidiator <insidiator@cmos.at>
2022-05-15 14:14:26 +0200

    fixed guild state not being initialized in some cases

--- src/history/GuildHistoryHelper.lua
+++ src/history/GuildHistoryHelper.lua
@@ -3,13 +3,21 @@ local AGS = AwesomeGuildStore
 local WrapFunction = AGS.internal.WrapFunction
 local RegisterForEvent = AGS.internal.RegisterForEvent
 local chat = AGS.internal.chat
-local GuildHistoryHelper = ZO_Object:Subclass()
+
+local GuildHistoryHelper = ZO_InitializingObject:Subclass()
 AGS.class.GuildHistoryHelper = GuildHistoryHelper
 
-function GuildHistoryHelper:New(...)
-    local wrapper = ZO_Object.New(self)
-    wrapper:Initialize(...)
-    return wrapper
+function GuildHistoryHelper:Initialize()
+    RegisterForEvent(EVENT_GUILD_HISTORY_CATEGORY_UPDATED, function(_, guildId, category)
+        if category == GUILD_HISTORY_STORE then
+            local guildState = self:GetOrInitializeGuildState(guildId)
+            local oldest, newest, endIndex = self:GetMinMaxPurchaseEventTimes(guildId, guildState.highestIndex)
+            guildState.hasMore = DoesGuildHistoryCategoryHaveMoreEvents(guildId, category)
+            guildState.highestIndex = endIndex
+            guildState.oldest = oldest
+            guildState.newest = newest
+        end
+    end)
 end
 
 function GuildHistoryHelper:GetNumPurchaseEvents(guildId)
@@ -19,7 +27,7 @@ end
 function GuildHistoryHelper:GetPurchaseEvent(guildId, index)
     local eventType, secsSinceEvent, sellerName, buyerName, itemCount, itemLink, sellPrice, tax = GetGuildEventInfo(guildId, GUILD_HISTORY_STORE, index)
     local subcategory = ComputeGuildHistoryEventSubcategory(eventType, GUILD_HISTORY_STORE)
-    if(subcategory == GUILD_HISTORY_STORE_PURCHASES) then
+    if subcategory == GUILD_HISTORY_STORE_PURCHASES then
         return eventType, secsSinceEvent, sellerName, buyerName, itemCount, itemLink, sellPrice, tax
     end
 end
@@ -30,7 +38,7 @@ function GuildHistoryHelper:GetMinMaxPurchaseEventTimes(guildId, startIndex)
     local oldest, newest = math.huge, 0
     for index = startIndex, endIndex do
         local eventType, secsSinceEvent = self:GetPurchaseEvent(guildId, index)
-        if(eventType) then
+        if eventType then
             local time = ZO_NormalizeSecondsSince(secsSinceEvent)
             oldest = math.min(oldest, time)
             newest = math.max(newest, time)
@@ -39,74 +47,34 @@ function GuildHistoryHelper:GetMinMaxPurchaseEventTimes(guildId, startIndex)
     return oldest, newest, endIndex
 end
 
-function GuildHistoryHelper:Initialize()
-    self:InitializeGuildState()
-
-    RegisterForEvent(EVENT_GUILD_HISTORY_CATEGORY_UPDATED, function(_, guildId, category)
-        if(category == GUILD_HISTORY_STORE) then
-            local guildState = self.state[guildId]
-            if(not guildState) then chat:Print("guildState not correctly initialized. Please report this to the author.") return end
-            local oldest, newest, endIndex = self:GetMinMaxPurchaseEventTimes(guildId, guildState.highestIndex)
-            guildState.hasMore = DoesGuildHistoryCategoryHaveMoreEvents(guildId, category)
-            guildState.highestIndex = endIndex
-            guildState.oldest = oldest
-            guildState.newest = newest
-        end
-    end)
-
-    RegisterForEvent(EVENT_GUILD_SELF_JOINED_GUILD, function(_, guildId, guildName)
-        self:InitializeGuildState()
-    end)
-    RegisterForEvent(EVENT_GUILD_SELF_LEFT_GUILD, function(_, guildId, guildName)
-        self:InitializeGuildState()
-    end)
-end
-
-function GuildHistoryHelper:InitializeGuildState()
-    local state = {}
-    for i = 1, GetNumGuilds() do
-        local guildId = GetGuildId(i)
+function GuildHistoryHelper:GetOrInitializeGuildState(guildId)
+    if not self.state[guildId] then
         local oldest, newest, endIndex = self:GetMinMaxPurchaseEventTimes(guildId)
-        state[guildId] = {
+        self.state[guildId] = {
             hasMore = DoesGuildHistoryCategoryHaveMoreEvents(guildId, GUILD_HISTORY_STORE),
             highestIndex = endIndex,
             oldest = oldest,
             newest = newest
         }
     end
-    self.state = state
+    return self.state[guildId]
 end
 
-function GuildHistoryHelper:RequestNewest(guildId)
-    if(RequestMoreGuildHistoryCategoryEvents(guildId, GUILD_HISTORY_STORE, true)) then
+function GuildHistoryHelper:RequestEvents(guildId)
+    if RequestMoreGuildHistoryCategoryEvents(guildId, GUILD_HISTORY_STORE, true) then
         return true
     end
     return false
-end
-
-function GuildHistoryHelper:RequestOlder(guildId)
-    if(RequestMoreGuildHistoryCategoryEvents(guildId, GUILD_HISTORY_STORE, true)) then
-        return true
-    end
-    return false
-end
-
-function GuildHistoryHelper:HandleReceivedData(guildId)
-    local guildState = self.state[guildId]
 end
 
 function GuildHistoryHelper:RequestData(normalizedTime)
     local hasRequestedData = false
     for i = 1, GetNumGuilds() do
         local guildId = GetGuildId(i)
-        local guildState = self.state[guildId]
-        if(not guildState.initialized or normalizedTime > guildState.newest) then
+        local guildState = self:GetOrInitializeGuildState(guildId)
+        if not guildState.initialized or (normalizedTime < guildState.oldest and guildState.hasMore) then
             guildState.initialized = true
-            if(self:RequestNewest(guildId)) then
-                hasRequestedData = true
-            end
-        elseif(normalizedTime < guildState.oldest and guildState.hasMore) then
-            if(self:RequestOlder(guildId)) then
+            if self:RequestEvents(guildId) then
                 hasRequestedData = true
             end
         end
