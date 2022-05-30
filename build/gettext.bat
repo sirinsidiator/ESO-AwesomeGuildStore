c8e9ef11451c72a9cdcd91e1ca3a656c282109cf
sirinsidiator <insidiator@cmos.at>
2022-05-30 23:16:44 +0200

    updated German translation

--- build/gettext.bat
+++ build/gettext.bat
@@ -3,5 +3,6 @@ echo Generating file list..
 dir ..\src\*.lua /L /B /S > %TEMP%\listfile.txt
 echo Generating .POT file...
 E:\programs\gettext\bin\xgettext -kgettext --from-code utf-8 --copyright-holder=sirinsidiator --package-name=AwesomeGuildStore --msgid-bugs-address=insidiator@cmos.at -cTRANSLATORS: -o ..\translations\messages.pot -L Lua --no-wrap -D ..\src -f %TEMP%\listfile.txt
+"C:\Program Files\PowerShell\7\pwsh.exe" -Command "(Get-Content ..\translations\messages.pot) -replace 'charset=CHARSET', 'charset=UTF-8' | Out-File ..\translations\messages.pot"
 echo Done.
 del %TEMP%\listfile.txt
---
+++ build/po2lua.bat
@@ -0,0 +1,4 @@
+pushd
+cd ..\translations
+E:\programs\Lua\5.3.5x64\lua53.exe ..\build\po2lua.lua
+popd
